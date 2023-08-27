f6ee4888c4491e3201d92f2130fe75b258fc5f39
sourav <souravsemail2001@gmail.com>
2023-08-28 00:42:45 +0530

    added the first project that I am making

---
+++ 03-pizza-menu/src/index.js
@@ -0,0 +1,126 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Pizza({ pizza }) {
+  return (
+    <li className="pizza">
+      <img src={pizza.photoName} alt={pizza.name} />
+      <div>
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>{pizza.price}</span>
+      </div>
+    </li>
+  );
+}
+
+function Menu() {
+  const pizzaComponents = pizzaData.map((pizza) => (
+    <Pizza key={pizza.name} pizza={pizza} />
+  ));
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">{pizzaComponents}</ul>
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  // alert(
+  //   hour >= openHour && hour <= closeHour
+  //     ? "The shop is open right now"
+  //     : "Sorry, but we're closed!"
+  // );
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      )}
+    </footer>
+  );
+  // return React.createElement(
+  //   "footer",
+  //   null,
+  //   React.createElement("h4", null, "We're currently open!")
+  // );
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Before react 18
+// React.render(<App />);
